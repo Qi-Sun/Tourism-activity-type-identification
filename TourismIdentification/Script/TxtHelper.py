1a42d5932fb4adbb0d49ec6fccdd2b003c9ea14b
Qi-Sun <qisun13@pku.edu.cn>
2019-12-21 08:51:04 +0800

    读取文本文件脚本

--- TourismIdentification/Script/TxtHelper.py
+++ TourismIdentification/Script/TxtHelper.py
@@ -11,4 +11,14 @@ def read_points_txt(filename):
             points.append([float(lat),float(lng)])
     return points
 
+def read_city_distance_csv(filename):
+    distance_hashmap = {}
+    with open(filename,'r',encoding='utf8') as rf:
+        for line in rf.readlines():
+            city,distance = line.strip('\r\n').split(',')
+            distance_hashmap[city] = float(distance)
+    return distance_hashmap
+
+
+
 
